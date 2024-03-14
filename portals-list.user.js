7e1658f3527271ad487a08220a8ed3e25db481ea
pzhfxuhfhc <pzhfxuhfhc@gmail.com>
2024-03-14 17:57:09 +0900

    hoge

--- portals-list.user.js
+++ portals-list.user.js
@@ -511,16 +511,3 @@ window.plugin.portalslist.portalApGainMaths = function (resCount, linkCount, fie
     captureAp: captureAp,
   };
 };
-
-setup.info = plugin_info; //add the script info data to the function as a property
-if(!window.bootPlugins) window.bootPlugins = [];
-window.bootPlugins.push(setup);
-// if IITC has already booted, immediately run the 'setup' function
-if(window.iitcLoaded && typeof setup === 'function') setup();
-} // wrapper end
-// inject code into site context
-var script = document.createElement('script');
-var info = {};
-if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
-script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
-(document.body || document.head || document.documentElement).appendChild(script);
